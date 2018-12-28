66a4dc730a0cedfaf0c7b19d590a3ee79f130e20
Ed <red@redde-Mac-mini.local>
2018-12-28 16:19:37 +0800

    add excel

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
-	"github.com/edfan0930/goed/cmd"
+	"fmt"
+
+	"github.com/edfan0930/goed/module/excel"
 )
 
-func main() {
+type uu struct {
+	Name string
+	Male string
+}
 
-	cmd.Execute()
+func main() {
+	u := []uu{uu{"ebabab", "man"}, uu{"ac", "female"}}
+
+	f := excel.NewExcel()
+	if err := f.FieldName("cp", []string{"name", "male"}); err != nil {
+		fmt.Println("17", err)
+	}
+	fmt.Println("uu", u)
+
+	for i := range u {
+		if err := f.WriteStruct(4, &u[i]); err != nil {
+			fmt.Println("23", err)
+			break
+		}
+	}
+
+	if err := f.Save("/Users/red/go/src/abab/test.xlsx"); err != nil {
+		fmt.Println("27", err)
+	}
+	//cmd.Execute()
 }
