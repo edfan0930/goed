75777f09a5f771deaf27185c673ac50dddee424b
Ed <edfan0930@gmail.com>
2020-11-20 22:47:43 +0800

    add file read method

--- module/file/read.go
+++ module/file/read.go
@@ -2,6 +2,11 @@ package file
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
@@ -35,4 +40,25 @@ func ReadFile() {
 	fmt.Println("port is", viper.GetInt("port"))
 	return
 
+}
+
+func FolderFiles() []string {
+	//取得執行檔的路徑,包含檔名
+	file, _ := exec.LookPath(os.Args[0])
+
+	//得到全路径，比如在windows下E:\\golang\\test\\a.exe
+	filePath, _ := filepath.Abs(file)
+	//只取資料夾
+	pwd := filepath.Dir(filePath)
+	fmt.Println("pwd", pwd)
+	files, err := ioutil.ReadDir(pwd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filesName := []string{}
+	for _, file := range files {
+		filesName = append(filesName, file.Name())
+	}
+
+	return filesName
 }
